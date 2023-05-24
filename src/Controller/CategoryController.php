2faf9c07dafe4873b447f9027065787a3b154b3f
ETIENNE_JOUAULT <etienne.jouaulmt@live.fr>
2023-05-24 13:42:37 +0200

    ended of quest10

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/category', name: 'category_')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/category/{categoryName}', name: 'category_show')]
+    public function show(string $categoryName, CategoryRepository $categoryRepository, ProgramRepository $programRepository ): Response
+    {
+        $category = $categoryRepository->findOneBy(['name' => $categoryName]);
+
+        if (!$category) {
+            throw $this->createNotFoundException('La catégorie n\'existe pas.');
+        }
+
+        $programs = $programRepository->findby(['category' => $category->getId()]);
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+            'programs' => $programs,
+        ]);
+    }
+
+}
