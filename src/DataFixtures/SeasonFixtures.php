1740441bc597419d95fdbeb5cd5894016e556ae7
ETIENNE_JOUAULT <etienne.jouaulmt@live.fr>
2023-06-12 10:57:39 +0200

    added fixture for episode and season

--- src/DataFixtures/SeasonFixtures.php
+++ src/DataFixtures/SeasonFixtures.php
@@ -7,19 +7,30 @@ use App\Entity\Season;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class SeasonFixtures extends Fixture implements DependentFixtureInterface
 {
-    public function load(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
-        $season = new Season();
-        $season->setNumber(1);
-        $season->setProgram($this->getReference('program_Lost'));
+        $faker = Factory::create();
 
-        $this->addReference('season1_Lost', $season);
-        $manager->persist($season);
+        for ($i = 1; $i <= 5; $i++) {
+            for ($j = 0; $j <= 5; $j++) {
+                $season = new Season();
+                //Ce Faker va nous permettre d'alimenter l'instance de Season que l'on souhaite ajouter en base
+                $season->setNumber($j);
+                $season->setYear($faker->year());
+                $season->setDescription($faker->paragraphs(3, true));
+                $season->setProgram($this->getReference('program_' . $i));
+                $manager->persist($season);
+                $this->addReference('program_'. $i . 'season_'. $j, $season);
+            }
+        }
         $manager->flush();
+
     }
+
 
     public function getDependencies()
     {
